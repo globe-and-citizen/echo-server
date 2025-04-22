89a1ba22296e0e552eb10db7b3ba7c2c3729acd1
thaodtp <dtpthao.its@gmail.com>
2025-04-22 14:04:42 +0700

    proxy: add sign & verify unit test

--- proxy/src/crypto/service.rs
+++ proxy/src/crypto/service.rs
@@ -44,4 +44,18 @@ impl EchoCrypto for CryptoService {
 }
 
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::crypto::EchoCrypto;
 
+    #[test]
+    fn test_sign_and_verify() {
+        let secret = b"my_secret_key";
+        let message = b"Hello, world!";
+        let crypto_service = CryptoService::new(secret);
+        let signature = crypto_service.sign_message(message);
+
+        assert!(crypto_service.verify_signature(message, &signature));
+    }
+}
