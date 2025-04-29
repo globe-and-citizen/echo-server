70d804fdb1eecb248b4c0a2f07590d6d37a27ceb
thaodtp <dtpthao.its@gmail.com>
2025-04-29 17:01:54 +0700

    proxy: remove OnceLock, add get_keypair method and update test cases

--- proxy/src/crypto/service.rs
+++ proxy/src/crypto/service.rs
@@ -1,27 +1,31 @@
 use ring::digest;
 use ring::signature::{Ed25519KeyPair, KeyPair};
-use std::sync::OnceLock;
 use crate::crypto::{EchoCrypto};
 
-#[derive(Default, Debug)]
+#[derive(Debug)]
 pub struct CryptoService {
-    locked_keypair: OnceLock<Ed25519KeyPair>,
+    keypair: Option<Ed25519KeyPair>, // Keypair is optional for testing purpose
 }
 
 impl CryptoService {
     pub fn new(secret: &[u8]) -> Self {
+        // This should throw a panic if the secret is empty but we let it be for now (testing purpose)
+        if secret.len() == 0 {
+            return Self {
+                keypair: None,
+            }
+        }
         // Hash the secret using SHA256
         let seed_bytes = digest::digest(&digest::SHA256, secret).to_owned();
-
-        // Create the keypair using the hashed secret
-        let keypair = Ed25519KeyPair::from_seed_unchecked(seed_bytes.as_ref()).expect("Failed to create keypair");
-
-        // Store the keypair in the OnceLock
-        let once_lock_keypair = OnceLock::new();
-        once_lock_keypair.get_or_init(|| { keypair });
-
         Self {
-            locked_keypair: once_lock_keypair,
+            keypair: Some(Ed25519KeyPair::from_seed_unchecked(seed_bytes.as_ref()).expect("Failed to create keypair"))
+        }
+    }
+
+    pub fn get_keypair(&self) -> Result<&Ed25519KeyPair, &'static str> {
+        match self.keypair {
+            Some(ref keypair) => Ok(keypair),
+            None => Err("Keypair not initialized"),
         }
     }
 }
@@ -29,13 +33,13 @@ impl CryptoService {
 impl EchoCrypto for CryptoService {
 
     fn sign_message(&self, message: &[u8]) -> Vec<u8> {
-        let keypair = self.locked_keypair.get().expect("Keypair not initialized");
+        let keypair = self.get_keypair().expect("Keypair not initialized");
         let signature = keypair.sign(message);
         signature.as_ref().to_vec()
     }
 
     fn verify_signature(&self, message: &[u8], signature: &[u8]) -> bool {
-        let keypair = self.locked_keypair.get().expect("Keypair not initialized");
+        let keypair = self.get_keypair().expect("Keypair not initialized");
         let public_key = keypair.public_key();
         let peer_public_key = ring::signature::UnparsedPublicKey::new(&ring::signature::ED25519, public_key.as_ref());
         peer_public_key.verify(message, signature).is_ok()
@@ -43,6 +47,7 @@ impl EchoCrypto for CryptoService {
 }
 
 
+// this is only for unittest practice, see proxy/tests/crypto.rs for more test coverage
 #[cfg(test)]
 mod tests {
     use super::*;
--- proxy/tests/crypto.rs
+++ proxy/tests/crypto.rs
@@ -1,11 +1,59 @@
 use proxy::crypto::{EchoCrypto, service::CryptoService};
 
 #[test]
-fn test_sign_and_verify() {
+fn sign_ok() {
     let secret = b"my_secret_key";
     let message = b"Hello, world!";
     let crypto_service = CryptoService::new(secret);
     let signature = crypto_service.sign_message(message);
 
+    assert_eq!(signature.len(), 64);
+    assert_eq!(hex::encode(&signature), "2990e2f07f205f76e04331b257a0c8369302ffc26b573d96b9100e3f4433b28bb427b1c62050aa49e5b8f1f7a25d9cb1df60b64001ee2a909a9c04e66042a905".to_string());
+}
+
+#[test]
+#[should_panic]
+fn sign_empty_secret() {
+    let secret = b"";
+    let message = b"Hello, world!";
+    let crypto_service = CryptoService::new(secret);
+    crypto_service.sign_message(message);
+}
+
+#[test]
+fn verify_ok() {
+    let secret = b"my_secret_key";
+    let message = b"Hello, world!";
+    let crypto_service = CryptoService::new(secret);
+    let signature = hex::decode("2990e2f07f205f76e04331b257a0c8369302ffc26b573d96b9100e3f4433b28bb427b1c62050aa49e5b8f1f7a25d9cb1df60b64001ee2a909a9c04e66042a905").unwrap();
     assert!(crypto_service.verify_signature(message, &signature));
+}
+
+#[test]
+#[should_panic]
+fn verify_empty_secret() {
+    let secret = b"";
+    let message = b"Hello, world!";
+    let crypto_service = CryptoService::new(secret);
+    let signature = hex::decode("2990e2f07f205f76e04331b257a0c8369302ffc26b573d96b9100e3f4433b28bb427b1c62050aa49e5b8f1f7a25d9cb1df60b64001ee2a909a9c04e66042a905").unwrap();
+    crypto_service.verify_signature(message, &signature);
+}
+
+#[test]
+fn verify_wrong_signature() {
+    let secret = b"my_secret_key";
+    let message = b"Hello, world!";
+    let crypto_service = CryptoService::new(secret);
+    let signature = hex::decode("6883452843fa7e6a58cf13509507bd36bc345bfff8bc97e226998eabb5e400fa2a5ec6bd65d2cd27f9a1ebafc3786028d802d7d58eca66bede0db7767e03ea09").unwrap();
+    assert!(!crypto_service.verify_signature(message, &signature));
+}
+
+#[test]
+#[should_panic]
+fn verify_invalid_signature_size() {
+    let secret = b"my_secret_key";
+    let message = b"Hello, world!";
+    let crypto_service = CryptoService::new(secret);
+    let signature = hex::decode("2990e2f07f205f76e04331b257a0c8369302ffc26b573d96b9100e3f4433b28bb427b1c62050aa49e5b8f1f7a25d9cb1df60b64001ee2a909a9c04e66042a90").unwrap();
+    crypto_service.verify_signature(message, &signature);
 }
